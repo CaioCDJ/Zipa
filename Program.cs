503f01731b448e28436cae8a44bb2df114800a54
CaioCDJ <caiodjesus1@hotmail.com>
2023-11-05 14:04:58 -0300

    Zip Files

--- Program.cs
+++ Program.cs
@@ -1,13 +1,53 @@
 ﻿using System.IO.Compression;
 using Spectre.Console;
 
+var paths = new List<Data>();
+
 // é o zip
 // ZipFile.CreateFromDirectory("./tozip/","zipado.zip");
 
 void zip(){
-  for(int i=1;i<=args.Length;i++){
-    
+  var lst = new List<string>();
+  string zipfileName;
+
+  while(true){
+    zipfileName = AnsiConsole.Ask<string>("Qual o nome do arquivo?");
+    if(!string.IsNullOrEmpty(zipfileName)){
+      
+      if(!File.Exists(zipfileName+".zip")) break;
+      
+      else  AnsiConsole.MarkupLine("[red]Este arquivo ja existe[/]");
+    }
+  } 
+
+  if(args is not null){ 
+    int lim = args.Length;
+    if(lim==1){
+      lst = directory();
+    }
   }
+  
+  Directory.CreateDirectory(zipfileName);
+  paths.ForEach(item=>{
+    Console.WriteLine(item.selected);
+    if(item.selected){
+      if(item.dir){
+    
+      }else
+        File.Copy(item.path,zipfileName+@"/"+item.name.Split(" ")[1]); 
+    }
+  });
+
+  ZipFile.CreateFromDirectory(zipfileName,zipfileName+".zip");
+  
+  foreach(string filesPaths in Directory.GetFiles(zipfileName))
+    File.Delete(filesPaths);
+
+  Directory.Delete(zipfileName);
+
+  Console.WriteLine("\n");
+  AnsiConsole.Write(new Rule());
+  AnsiConsole.MarkupLine($"[green]{zipfileName}[/] foi gerado");
 }
 
 void unzip(){
@@ -28,7 +68,6 @@ void unzip(){
     }
     i++;
   }
-  Console.WriteLine(file.Directory.FullName +@"/"+name);
 
   ZipFile.ExtractToDirectory(
       file.FullName,
@@ -39,16 +78,17 @@ void unzip(){
 
 }
 
-void directory(){
-
-  var lst = new List<Data>();
-
+List<string> directory(){
   
   var dirs = Directory.GetDirectories(Directory.GetCurrentDirectory());
 
   for(int i =0;i< dirs.Length;i++){
     var dir = new DirectoryInfo(dirs[i]).Name; 
-    lst.Add(new Data( dirs[i],$"[yellow][/] {dir}", true));
+    paths.Add(new Data( ){
+        path= dirs[i],
+        name = $"[yellow][/] {dir}",
+        dir = true
+        });
   }
 
   var files = Directory.GetFiles(Directory.GetCurrentDirectory());
@@ -56,20 +96,39 @@ void directory(){
   for(int i =0;i < files.Length;i++){
     
     var name = new FileInfo(files[i]).Name;
-    
-    lst.Add(new Data( files[i], $"[blue]󰈙[/]{name}", false));
+    paths.Add(new Data(){
+        path = files[i],
+        name =  $"[blue]󰈙[/] {name}",
+        dir = false
+        });  
+    // paths.Add(new Data( files[i], $"[blue]󰈙[/] {name}", false));
   }
   
   var selected = AnsiConsole.Prompt(
       new MultiSelectionPrompt<string>()
         .Title("Quais arquivos devo compactar")
         .PageSize(30)
-        .AddChoices(lst.Select(x=>x.nome).ToArray())
+        .AddChoices(paths.Select(x=>x.name).ToArray())
       );
-  Console.Write("\n");
-  foreach(var i in selected) AnsiConsole.MarkupLine($"[green]{i}[/]");  
-}
+  // Console.Write("\n");
+  // foreach(var i in selected) AnsiConsole.MarkupLine($"[green]{i}[/]");  
 
+  
+  for(int i =0;i<=(paths.Count-1);i++){
+    foreach(string item in selected){
+
+      if(paths[i].name == item){
+        paths[i] = new Data(){
+            name=paths[i].name,
+            path=paths[i].path,
+            dir = paths[i].dir,
+            selected = true
+          };  
+      }
+    }
+  }
+  return selected;
+}
 
 if(args.Length>0){
 // if(args[0] is not null || !String.IsNullOrEmpty(args[0])){
@@ -85,4 +144,9 @@ if(args.Length>0){
 }else
   directory();
 
-public record Data(string path, string nome, bool dir);
+public class Data{
+  public string path;
+  public string name;
+  public bool dir {get;set;}=false;
+  public bool selected {get;set;}=false;
+}
